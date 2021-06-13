e423914fad33d954c4f6defefeeefe80b499ea14
Renan Kanu <renankanu@gmail.com>
2021-06-13 15:26:39 -0300

    create folder config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Renan Kanu',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
