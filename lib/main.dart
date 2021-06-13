2d796ea4f6e739263b784181b5d62732d4b70364
Renan Kanu <renankanu@gmail.com>
2021-06-13 17:27:26 -0300

    init getx structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "<RenanKanu>",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
