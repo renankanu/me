6bee949a25e561a9ecdcc43efec2bd90fe0a63df
Renan Kanu <renankanu@gmail.com>
2021-06-26 11:26:16 -0300

    add image in background

--- lib/app/sections/about/about_section.dart
+++ lib/app/sections/about/about_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:me/app/core/core.dart';
 import 'package:me/app/widgets/base_section_container.dart';
 import 'package:me/app/widgets/title_section.dart';
 import 'package:me/generated/locales.g.dart';
@@ -18,53 +19,57 @@ class AboutSection extends StatelessWidget {
           children: [
             TitleSection(titleSection: LocaleKeys.menu_about.tr),
             SizedBox(height: 180),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AvatarImage(),
-                SizedBox(width: 30),
-                Expanded(
-                  child: Container(
-                    child: Column(
-                      children: [
-                        Text(
-                          LocaleKeys.aboutSection_aboutMeDescOne.tr,
-                          style: Get.textTheme.headline5?.copyWith(
-                            fontSize: 48,
-                            letterSpacing: 1.5,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 12),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(12),
-                              ),
-                              color: Get.theme.accentColor,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 12,
-                                horizontal: 24,
-                              ),
-                              child: Text(LocaleKeys
-                                  .aboutSection_tagMobileDeveloper.tr),
+            Visibility(
+              visible: !CustomizedResponsive.isSmallScreen(context),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AvatarImage(),
+                  SizedBox(width: 30),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            LocaleKeys.aboutSection_aboutMeDescOne.tr,
+                            style: Get.textTheme.headline5?.copyWith(
+                              fontSize: 48,
+                              letterSpacing: 1.5,
                             ),
                           ),
-                        ),
-                        Text(
-                          LocaleKeys.aboutSection_aboutMeDescTwo.tr,
-                          style: Get.textTheme.headline2?.copyWith(
-                            fontSize: 20,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(12),
+                                ),
+                                color: Get.theme.accentColor,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 12,
+                                  horizontal: 24,
+                                ),
+                                child: Text(LocaleKeys
+                                    .aboutSection_tagMobileDeveloper.tr),
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
+                          Text(
+                            LocaleKeys.aboutSection_aboutMeDescTwo.tr,
+                            style: Get.textTheme.headline2?.copyWith(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+              replacement: Container(),
+            )
           ],
         ),
       ),
