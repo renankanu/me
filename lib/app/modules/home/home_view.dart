22434afce1106b2ab1aeb615fdbfc0df53dc649f
Renan Kanu <renankanu@gmail.com>
2021-07-03 13:28:39 -0300

    finish repo section, add loading initial

--- lib/app/modules/home/home_view.dart
+++ lib/app/modules/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:loading_animations/loading_animations.dart';
+import 'package:me/app/core/core.dart';
 import 'package:me/app/sections/about/about_section.dart';
 import 'package:me/app/sections/intro/intro_section.dart';
 import 'package:me/app/sections/repo/repo_section.dart';
@@ -51,9 +52,25 @@ class HomeView extends GetView<HomeController> {
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'RK;',
-                    style: Get.textTheme.headline4,
+                  RichText(
+                    text: TextSpan(
+                      text: 'R',
+                      style: Get.textTheme.headline1?.copyWith(
+                        fontWeight: FontWeight.w900,
+                        color: CustomizedColors.white,
+                        fontSize: 26,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'K',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 26,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                   SizedBox(height: 20),
                   LoadingBouncingGrid.square(
