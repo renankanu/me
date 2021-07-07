14c5ef8dfebbfc935421d489298d9fe99677d47a
renankanu <renankanu@gmail.com>
2021-07-07 00:35:22 -0300

    create light theme

--- lib/app/core/colors.dart
+++ lib/app/core/colors.dart
@@ -9,13 +9,15 @@ abstract class CustomizedColors {
   static const menu = Color(0xFF212225);
   static const magicMint = Color(0xFF96F3B2);
   static const vistaBlue = Color(0xFF80D799);
-  static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF000000);
   static const texas = Color(0xFFF5F8A8);
   static const frenchPass = Color(0xFFACEBFC);
   static const casablanca = Color(0xFFF4BE4F);
   static const mantis = Color(0xFF62C655);
   static const silver = Color(0xFFCCCCCC);
+
+  //NEW THEME
+  static const white = Color(0xFFFFFFFF);
   static const alto = Color(0xFFDFDFDF);
 
   //NEW THEME
--- lib/app/theme/themes.dart
+++ lib/app/theme/themes.dart
@@ -8,10 +8,74 @@ class Themes {
     brightness: Brightness.light,
     primaryColor: Colors.white,
     accentColor: CustomizedColors.burntSienna,
-    textTheme: GoogleFonts.rubikTextTheme().copyWith(
-      bodyText2: TextStyle(color: CustomizedColors.ebonyClay),
-    ),
+    scaffoldBackgroundColor: CustomizedColors.white,
+    hintColor: CustomizedColors.alto,
+    shadowColor: CustomizedColors.black.withOpacity(0.5),
+    dividerColor: CustomizedColors.alto.withOpacity(0.5),
+    cardColor: CustomizedColors.ebonyClay,
     disabledColor: CustomizedColors.ebonyClay,
+    textTheme: GoogleFonts.rubikTextTheme().copyWith(
+      headline1: GoogleFonts.rubik(
+        fontSize: 96,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -1.5,
+        color: CustomizedColors.ebonyClay,
+      ),
+      headline2: GoogleFonts.rubik(
+        fontSize: 60,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -0.5,
+        color: CustomizedColors.ebonyClay,
+      ),
+      headline3: GoogleFonts.rubik(
+        fontSize: 48,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+        color: CustomizedColors.ebonyClay,
+      ),
+      headline4: GoogleFonts.rubik(
+        fontSize: 34,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+        color: CustomizedColors.ebonyClay,
+      ),
+      headline5: GoogleFonts.rubik(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        letterSpacing: 0,
+        color: CustomizedColors.ebonyClay,
+      ),
+      headline6: GoogleFonts.rubik(
+        fontSize: 20,
+        fontWeight: FontWeight.w900,
+        letterSpacing: 0.15,
+        color: CustomizedColors.ebonyClay,
+      ),
+      subtitle1: GoogleFonts.rubik(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.15,
+        color: CustomizedColors.ebonyClay,
+      ),
+      subtitle2: GoogleFonts.rubik(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.1,
+        color: CustomizedColors.ebonyClay,
+      ),
+      bodyText1: GoogleFonts.rubik(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.5,
+        color: CustomizedColors.ebonyClay,
+      ),
+      bodyText2: GoogleFonts.rubik(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+        color: CustomizedColors.ebonyClay,
+      ),
+    ),
   );
 
   final darkTheme = ThemeData.dark().copyWith(
