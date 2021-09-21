28b06ba75fc91b73083c0f333c85ee19dca14e9b
renankanu <renankanu@gmail.com>
2021-09-21 16:37:30 -0300

    refactor name date formatter

--- lib/app/sections/repo/widgets/card_pinned.dart
+++ lib/app/sections/repo/widgets/card_pinned.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:me/app/core/colors.dart';
-import 'package:me/app/utils/date_formater.dart';
+import 'package:me/app/utils/date_formatter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class CardPinned extends StatelessWidget {
