d04efe11dcd5367edcc6c348f19c14ad2197c085
renankanu <renankanu@gmail.com>
2021-12-20 12:09:34 -0300

    feature: add hat for merry christmas

--- lib/app/widgets/avatar_images.dart
+++ lib/app/widgets/avatar_images.dart
@@ -26,14 +26,20 @@ class AvatarImage extends StatelessWidget {
       width: size ?? getSize(),
       child: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(1),
-            child: Image.asset(
-              CustomizedImages.me,
-              semanticLabel: 'Avatar me',
+          Container(
+            height: (size ?? getSize()) - 18,
+            width: (size ?? getSize()) - 18,
+            child: Padding(
+              padding: const EdgeInsets.all(1),
+              child: Image.asset(
+                CustomizedImages.me,
+                semanticLabel: 'Avatar me',
+              ),
             ),
           ),
           Container(
+            height: (size ?? getSize()) - 18,
+            width: (size ?? getSize()) - 18,
             decoration: BoxDecoration(
               border: Border.all(
                 width: borderWidth ?? 4,
@@ -41,6 +47,18 @@ class AvatarImage extends StatelessWidget {
               ),
               borderRadius: BorderRadius.all(
                 Radius.circular(getSize() / 2),
+              ),
+            ),
+          ),
+          Positioned(
+            top: CustomizedResponsive.isSmallScreen() ? 0 : -12,
+            right: CustomizedResponsive.isSmallScreen() ? 10 : -8,
+            child: RotationTransition(
+              turns: const AlwaysStoppedAnimation(38 / 360),
+              child: Container(
+                height: CustomizedResponsive.isSmallScreen() ? 40 : 94,
+                width: CustomizedResponsive.isSmallScreen() ? 40 : 94,
+                child: Image.asset(CustomizedImages.hat),
               ),
             ),
           ),
